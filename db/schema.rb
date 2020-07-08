fb841052bd80c4e9b2429efcfbbeede39c0cd505
Hazar Baloom <hazar.baloom@gmail.com>
2020-07-08 19:07:01 +0000

    Added proiles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200701180419) do
+ActiveRecord::Schema.define(version: 20200708190236) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20200701180419) do
     t.decimal  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,7 +55,6 @@ ActiveRecord::Schema.define(version: 20200701180419) do
     t.integer  "plan_id"
     t.string   "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
-     t.index ["plan_id"], name: "index_users_on_plan_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
